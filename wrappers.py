10d6f3ced64fd950b40b4742bab1ef3a71c28e33
AlexanderBlad <sebbem30@hotmail.se>
2020-06-26 16:32:00 +0200

    Removed some unwanted features

--- wrappers.py
+++ wrappers.py
@@ -275,7 +275,7 @@ def make_atari(env_id, max_episode_steps=None):
         env = TimeLimit(env, max_episode_steps=max_episode_steps)
     return env
 
-def wrap_deepmind(env, episode_life=True, clip_rewards=True, frame_stack=False, scale=False):
+def wrap_deepmind(env, episode_life=True, clip_rewards=True, frame_stack=False):
     """Configure environment for DeepMind-style Atari.
     """
     if episode_life:
@@ -283,10 +283,6 @@ def wrap_deepmind(env, episode_life=True, clip_rewards=True, frame_stack=False, 
     if 'FIRE' in env.unwrapped.get_action_meanings():
         env = FireResetEnv(env)
     env = WarpFrame(env)
-    if scale:
-        env = ScaledFloatFrame(env)
-    if clip_rewards:
-        env = ClipRewardEnv(env)
     if frame_stack:
         env = FrameStack(env, 4)
     return env
