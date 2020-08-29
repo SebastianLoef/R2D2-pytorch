d8060ee7bc7d3d72fa4111667b426d490b3c0e8b
SebastianLoef <sebastian@loef.se>
2020-08-30 01:58:40 +0200

    added functionality get_sample

--- replaymemory.py
+++ replaymemory.py
@@ -1,7 +1,9 @@
-import random
-from heapq import heappush, heappop, heapify
+"""
+    TODO: add locked leafs functionality and add ability to update p for leafs
+"""
+import numpy as np
 
-import torch
+
 class Node():
     def __init__(self, p=0, data=None):
         self.p = p
@@ -15,6 +17,7 @@ class Node():
 class SumTree():
     def __init__(self, size):
         self.leafs = [Node() for _ in range(size)]
+        self.crown = None
         self.max_size = size
         self.index = 0
 
@@ -24,10 +27,7 @@ class SumTree():
                 if former_node is None:
                     node.parent = Node()
                     node.parent.left = node
-                    former_node = node
-                    continue
-
-                if former_node.parent.right is None:
+                elif former_node.parent.right is None:
                     former_node.parent.right = node
                     node.parent = former_node.parent
                 else:
@@ -37,6 +37,8 @@ class SumTree():
             new_nodes = set(node.parent for node in unique_nodes)
             if len(new_nodes) > 1:
                 fill_parents(new_nodes)
+            else:
+                self.crown = new_nodes.pop()
 
         fill_parents(self.leafs)
 
@@ -48,16 +50,16 @@ class SumTree():
 
         self.root = get_root()
 
-    def update_branch(self, node, dp):
-        node.p += dp 
+    def update_branch(self, node, delta_p):
+        node.p += delta_p
         if node.parent is not None:
-            self.update_branch(node.parent, dp)
+            self.update_branch(node.parent, delta_p)
 
     def append(self, p, data):
-        diff = p - self.leafs[self.index].p
+        delta_p = p - self.leafs[self.index].p
         self.leafs[self.index].data = data
         self.leafs[self.index].p = p
-        self.update_branch(self.leafs[self.index].parent, diff)
+        self.update_branch(self.leafs[self.index].parent, delta_p)
 
         self.index = (self.index + 1) % self.max_size
 
@@ -66,16 +68,19 @@ class SumTree():
             p, data = sequence
             self.append(p, data)
 
-    def get_batch(self, batch_size):
-        pass
+    def _find(self, p, node, offset=0):
+        if node.data is not None:
+            return node.data
+        if node.left is None:
+            return self._find(p, node.right, offset)
+        if p >= node.left.p+offset:
+            return self._find(p, node.right, node.left.p+offset)
+        else:
+            return self._find(p, node.left, offset)
 
-
-
-    
-        
-
-
-    
-
-
-
+    def get_sample(self, sample_size):
+        p_total = self.crown.p
+        bins = np.linspace(0, p_total, sample_size+1)
+        sampled_values = np.random.rand(sample_size)
+        sampled_values = (bins[1:] - bins[:-1]) * sampled_values + bins[:-1]
+        return [self._find(p, self.crown) for p in sampled_values]
